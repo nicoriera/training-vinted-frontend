a9bb937589150637e79a21ac3bf487ef8c1a822e
nicoriera <80978348+nicoriera@users.noreply.github.com>
2021-07-24 16:17:02 +0200

    add css

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="container-login">
+      <div className="block-login">
+        <div>Se connecter</div>
+        <form action="">
+          <input type="text" placeholder="Adresse email" />
+          <input type="text" placeholder="Mot de passe" />
+        </form>
+      </div>
+      <div button-login>
+        <button>Se connecter</button>
+        <p>Pas encore de compte ? Inscris-toi!</p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
